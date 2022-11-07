de55cdccff72f07d398469a0a21ef1841dca2e2f
attx <attx@localhost>
2022-11-07 07:09:27 +0100

    addlibs.bat

---
+++ addlibs.bat
@@ -0,0 +1,8 @@
+rem pip problemak megoldasa: CCleaner: delete system temp files
+
+call python.bat -m ensurepip
+call python.bat -m pip install sympy
+call python.bat -m pip install mathmatrix
+call python.bat -m pip install eseries
+call python.bat -m pip install numpy
+call python.bat -m pip install matplotlib
